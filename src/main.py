2879b5683eab2d5751be61b38450157876ba0c81
pojgik <luke.dinkla@gmail.com>
2025-05-15 16:34:18 -0400

    Added static copying

--- src/main.py
+++ src/main.py
@@ -1,11 +1,32 @@
 from textnode import *
 from htmlnode import *
+import os
+import shutil
+
+
+def copy_static(source, target):
+    if os.path.exists(target):
+        shutil.rmtree(target)
+        os.mkdir(target)
+    copy_directory(source, target)
+            
+def copy_directory(source, target):
+    if not os.path.exists(target):
+        os.mkdir(target)
+    
+    for item in os.listdir(source):
+        source_item = os.path.join(source, item)
+        target_item = os.path.join(target, item)
+        
+        if os.path.isfile(source_item):
+            shutil.copy(source_item, target_item)
+            print(f"Copied: {source_item} to {target_item}")
+        else:
+            copy_static(source_item, target_item)
+
 
 def main():
-    node = TextNode('This is some anchor text', TextType.LINK, 'https://www.boot.dev')
-    print(node.__repr__())
-    htmlNode = HTMLNode()
-    print(htmlNode.__repr__())
+    copy_static("static", "public")
     
 if __name__ == '__main__':
     main()
